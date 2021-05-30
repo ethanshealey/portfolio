38d8d1da045161f3d3ee1f6d4c284e2c1beb9cc4
Ethan Shealey <ethan.shealey@gmail.com>
2021-05-30 16:13:01 -0400

    Push

--- app/forms.py
+++ app/forms.py
@@ -1,10 +1,10 @@
 from flask_wtf import FlaskForm
-from wtforms import Form as NoCsrfForm, StringField, PasswordField, BooleanField, SubmitField, TextAreaField, SelectMultipleField, IntegerField, FieldList, FormField
-from wtforms.validators import DataRequired, Length
+from wtforms import StringField, TextAreaField, SubmitField
+from wtforms.validators import DataRequired, Length, Email
 from wtforms.widgets import HiddenInput
 
 class ContactForm(FlaskForm):
     name = StringField(validators=[DataRequired()])
-    email = StringField(validators=[DataRequired()])
+    email = StringField(validators=[DataRequired(), Email()])
     message = TextAreaField(validators=[DataRequired()])
     submit = SubmitField()
--- config.py
+++ config.py
@@ -3,9 +3,9 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-    MAIL_SERVER = os.environ.get('MAIL_SERVER')
-    MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
-    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
-    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
-    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-    ADMINS = ['ethanshealey@gmail.com']
+    MAIL_SERVER = 'smtp.gmail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = 1
+    MAIL_USERNAME = 'homemadedub@gmail.com'
+    MAIL_PASSWORD = 'mnpdapcxrvnlcclb'
+    ADMINS = ['ethan.shealey@gmail.com']
